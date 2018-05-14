08bf0d6623a798ca26cb5025c712cdb19d86cea7
alonglo361 <alonglo361@nemi.tskoli.is>
2018-05-14 16:47:58 +0000

    Add files via upload

--- addon/appdata.js
+++ addon/appdata.js
@@ -13,66 +13,66 @@ $.getJSON('https://alonglo361.github.io/addon/algorithms.json', function(data) {
 });
 
 
-function createElements(){ 
-	for (let i = 0; i < data['results'].length; i++){
+	function createElements(){ 
+		for (let i = 0; i < allAlgorithms.length; i++){
 
-		let algoBox = document.createElement('div'); //This is the box that encases 1 algorithm.
-		algoBox.setAttribute('class', allAlgorithms[i]['row']);
-		algoBox.setAttribute('id', allAlgorithms[i]['algorithmName']);
+			let algoBox = document.createElement('div'); //This is the box that encases 1 algorithm.
+			algoBox.setAttribute('class', allAlgorithms[i]['row']);
+			algoBox.setAttribute('id', allAlgorithms[i]['algorithmName']);
 
-		let boxImage = document.createElement('img');
-		boxImage.setAttribute('src', allAlgorithms[i]['img']);
-		algoBox.appendChild(boxImage);
+			let boxImage = document.createElement('img');
+			boxImage.setAttribute('src', allAlgorithms[i]['img']);
+			algoBox.appendChild(boxImage);
 
-		let infoElem = document.createElement('div');
-		infoElem.setAttribute('class', 'infoDiv');
-		algoBox.appendChild(infoElem);
+			let infoElem = document.createElement('div');
+			infoElem.setAttribute('class', 'infoDiv');
+			algoBox.appendChild(infoElem);
 
-		let infoName = document.createElement('div');
-		let strong = document.createElement('strong');
-		let strongName = document.createTextNode(allAlgorithms[i]['algorithmName']);
-		infoName.appendChild(strong);
-		infoName.appendChild(strongName);
-		infoElem.appendChild(infoName);
+			let infoName = document.createElement('div');
+			let strong = document.createElement('strong');
+			let strongName = document.createTextNode(allAlgorithms[i]['algorithmName']);
+			infoName.appendChild(strong);
+			infoName.appendChild(strongName);
+			infoElem.appendChild(infoName);
 
-		holdAlgorithms.appendChild(algoBox);
+			holdAlgorithms.appendChild(algoBox);
 
-		document.body.appendChild(holdAlgorithms);
-	}
-}
-
-	function searchFunction() { 
-		var selectAllAlgorithms = document.querySelectorAll('#algorithms .algorithm');    
-		var search = document.querySelector('#filter-search');
-		var cache = [];
-
-		selectAllAlgorithms.forEach(function(one_algorithm) {
-			cache.push({
-			  element: one_algorithm,
-			  text: one_algorithm.id.trim().toLowerCase()
-			});
-		});
-
-		function filter() {
-		var query = this.value.trim().toLowerCase();
-			cache.forEach(function(algorithmText) {
-			  var index = 0;
-
-			  if (query) {
-			    index = algorithmText.text.indexOf(query);
-			  }
-
-			  algorithmText.element.style.display = index === -1 ? 'none' : '';
-			});
-		}
-
-		if ('oninput' in search) {
-			search.addEventListener('input', filter);
-		} 
-		else {
-			search.addEventListener('keyup', filter);
+			document.body.appendChild(holdAlgorithms);
 		}
 	}
+
+		function searchFunction() { 
+			var selectAllAlgorithms = document.querySelectorAll('#algorithms .algorithm');    
+			var search = document.querySelector('#filter-search');
+			var cache = [];
+
+			selectAllAlgorithms.forEach(function(one_algorithm) {
+				cache.push({
+				  element: one_algorithm,
+				  text: one_algorithm.id.trim().toLowerCase()
+				});
+			});
+
+			function filter() {
+			var query = this.value.trim().toLowerCase();
+				cache.forEach(function(algorithmText) {
+				  var index = 0;
+
+				  if (query) {
+				    index = algorithmText.text.indexOf(query);
+				  }
+
+				  algorithmText.element.style.display = index === -1 ? 'none' : '';
+				});
+			}
+
+			if ('oninput' in search) {
+				search.addEventListener('input', filter);
+			} 
+			else {
+				search.addEventListener('keyup', filter);
+			}
+		}
 
 	createElements();
 	searchFunction();
