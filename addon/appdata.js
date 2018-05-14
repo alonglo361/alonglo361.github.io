47ca3684f61f286eb94feb2c2c7ac1f0be93f501
alonglo361 <alonglo361@nemi.tskoli.is>
2018-05-14 15:11:37 +0000

    Add files via upload

---
+++ addon/appdata.js
@@ -0,0 +1,44 @@
+"use strict";
+
+let holdAlgorithms = document.createElement('span');
+holdAlgorithms.setAttribute('id', 'algorithm');
+
+let allAlgorithms = [];
+// let sortByName;
+// let sortByPlace;
+
+$.getJSON('algorithms.json', function(data) {
+	for (let i = 0; i < data['results'].length; i++){ 
+    	allAlgorithms.push({algorithmName: data['results'][i]['name'], img: data['results'][i]['imageSource'], row: data['results'][i]['row']});
+    }
+});
+
+
+
+function createElements(){ 
+	for (let x = 0; x < data['results'].length; x++){
+
+		let algoBox = document.createElement('div'); //This is the box that encases 1 algorithm.
+		algoBox.setAttribute('class', allAlgorithms[x]['row']);
+		algoBox.setAttribute('id', allAlgorithms[x]['algorithmName']);
+
+		let boxImage = document.createElement('img');
+		boxImage.setAttribute('src', allAlgorithms[x]['img']);
+		algoBox.appendChild(boxImage);
+
+		let infoElem = document.createElement('div');
+		infoElem.setAttribute('class', 'infoDiv');
+		algoBox.appendChild(infoElem);
+
+		let infoName = document.createElement('div');
+		let strong = document.createElement('strong');
+		let strongName = document.createTextNode(allAlgorithms[x]['name']);
+		infoName.appendChild(strong);
+		infoName.appendChild(strongName);
+		infoElem.appendChild(infoName);
+
+		holdAlgorithms.appendChild(algoBox);
+
+		document.body.appendChild(holdAlgorithms);
+	}
+}
