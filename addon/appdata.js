570629e4953edd5dba45d49ef62a28e61f0f219c
alonglo361 <alonglo361@nemi.tskoli.is>
2018-05-14 16:20:56 +0000

    Add files via upload

--- addon/appdata.js
+++ addon/appdata.js
@@ -1,11 +1,9 @@
 "use strict";
 
 let holdAlgorithms = document.createElement('span');
-holdAlgorithms.setAttribute('id', 'algorithm');
+holdAlgorithms.setAttribute('id', 'algorithms');
 
 let allAlgorithms = [];
-// let sortByName;
-// let sortByPlace;
 let ex = "gay";
 $.getJSON('https://alonglo361.github.io/addon/algorithms.json', function(data) {
 	ex = "gaytony";
@@ -16,14 +14,14 @@ $.getJSON('https://alonglo361.github.io/addon/algorithms.json', function(data) {
 
 
 function createElements(){ 
-	for (let x = 0; x < data['results'].length; x++){
+	for (let i = 0; x < data['results'].length; i++){
 
 		let algoBox = document.createElement('div'); //This is the box that encases 1 algorithm.
-		algoBox.setAttribute('class', allAlgorithms[x]['row']);
-		algoBox.setAttribute('id', allAlgorithms[x]['algorithmName']);
+		algoBox.setAttribute('class', allAlgorithms[i]['row']);
+		algoBox.setAttribute('id', allAlgorithms[i]['algorithmName']);
 
 		let boxImage = document.createElement('img');
-		boxImage.setAttribute('src', allAlgorithms[x]['img']);
+		boxImage.setAttribute('src', allAlgorithms[i]['img']);
 		algoBox.appendChild(boxImage);
 
 		let infoElem = document.createElement('div');
@@ -32,7 +30,7 @@ function createElements(){
 
 		let infoName = document.createElement('div');
 		let strong = document.createElement('strong');
-		let strongName = document.createTextNode(allAlgorithms[x]['name']);
+		let strongName = document.createTextNode(allAlgorithms[i]['algorithmName']);
 		infoName.appendChild(strong);
 		infoName.appendChild(strongName);
 		infoElem.appendChild(infoName);
@@ -42,3 +40,39 @@ function createElements(){
 		document.body.appendChild(holdAlgorithms);
 	}
 }
+
+	function searchFunction() { 
+		var selectAllAlgorithms = document.querySelectorAll('#algorithms .algorithm');    
+		var search = document.querySelector('#filter-search');
+		var cache = [];
+
+		selectAllAlgorithms.forEach(function(one_algorithm) {
+			cache.push({
+			  element: one_algorithm,
+			  text: one_algorithm.id.trim().toLowerCase()
+			});
+		});
+
+		function filter() {
+		var query = this.value.trim().toLowerCase();
+			cache.forEach(function(algorithmText) {
+			  var index = 0;
+
+			  if (query) {
+			    index = algorithmText.text.indexOf(query);
+			  }
+
+			  algorithmText.element.style.display = index === -1 ? 'none' : '';
+			});
+		}
+
+		if ('oninput' in search) {
+			search.addEventListener('input', filter);
+		} 
+		else {
+			search.addEventListener('keyup', filter);
+		}
+	}
+
+	createElements();
+	searchFunction();
